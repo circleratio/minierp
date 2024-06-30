c7b7b6996d2bc67d7c0399a3b8ebfa5228c30b35
circleratio <tf@dsl.gr.jp>
2024-06-30 14:24:37 +0900

    refactor

--- people.py
+++ people.py
@@ -11,20 +11,19 @@ class People:
         for row in self.db.get('people'):
             print(row)
 
+    def add(self, data):
+        newid = data['name']
+        if self.has(newid):
+            print(f'People.add: the use already exists: {newid}')
+        else:
+            self.db.set('people', data)
+            self.db.commit()
+            
     def has(self, name):
-        print(name)
         where = {'name': name}
         for row in self.db.get('people', where):
             return True
         return False
-
-    def add(self, data):
-        newid = data['name']
-        if self.has(newid):
-            print('already exists')
-        else:
-            self.db.set('people', data)
-            self.db.commit()
 
 def main():
     db_people = People()
